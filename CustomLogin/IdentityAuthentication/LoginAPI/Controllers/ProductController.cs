f56402ef61d1149eaa8de8b8e6d7c1dd805e1ca9
Theodor Risager <theodor349@gmail.com>
2021-01-13 19:36:40 +0100

    API Call

---
+++ CustomLogin/IdentityAuthentication/LoginAPI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public Product GetProduct()
+        {
+            return new Product() { Id = 1, Price = 10 };
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+    }
+}
