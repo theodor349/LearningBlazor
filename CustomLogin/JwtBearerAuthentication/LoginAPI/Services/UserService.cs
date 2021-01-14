fb910fc92b94fbd9229a13aaa787b3a983b87c49
Theodor Risager <theodor349@gmail.com>
2021-01-14 09:44:05 +0100

    JWT User Controller

---
+++ CustomLogin/JwtBearerAuthentication/LoginAPI/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using LoginAPI.Entities;
+using LoginAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAPI.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model, string ipAddress);
+        AuthenticateResponse RefreshToken(string token, string ipAddress);
+        bool RevokeToken(string token, string ipAddress);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+
+    public class UserService
+    {
+    }
+}
