d353fc59da36de8542723c518bfabb32c036c6f1
Theodor Risager <theodor349@gmail.com>
2021-01-15 15:03:13 +0100

    User Service

---
+++ CustomLogin/JwtBearerAuthentication/LoginAPI/Helpers/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using LoginAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAPI.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
--- CustomLogin/JwtBearerAuthentication/LoginAPI/Services/UserService.cs
+++ CustomLogin/JwtBearerAuthentication/LoginAPI/Services/UserService.cs
@@ -1,8 +1,15 @@
 ﻿using LoginAPI.Entities;
+using LoginAPI.Helpers;
 using LoginAPI.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace LoginAPI.Services
@@ -16,7 +23,112 @@ namespace LoginAPI.Services
         User GetById(int id);
     }
 
-    public class UserService
+    public class UserService : IUserService
     {
+        private DataContext _context;
+        private readonly AppSettings _appSettings;
+
+        public UserService(DataContext context, IOptions<AppSettings> appSettings)
+        {
+            _context = context;
+            _appSettings = appSettings.Value;
+        }
+
+        public AuthenticateResponse Authenticate(AuthenticateRequest model, string ipAddress)
+        {
+            var user = _context.Users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+
+            if (user is null) return null;
+
+            var jwtToken = GenerateJwtToken(user);
+            var refreshTotken = GenerateRefreshToken(ipAddress);
+
+            user.RefreshTokens.Add(refreshTotken);
+            _context.Update(user);
+            _context.SaveChanges();
+
+            return new AuthenticateResponse(user, jwtToken, refreshTotken.Token);
+        }
+
+        public AuthenticateResponse RefreshToken(string token, string ipAddress)
+        {
+            var user = _context.Users.SingleOrDefault(user => user.RefreshTokens.Any(testc => testc.Token == token));
+            if (user is null) return null;
+
+            var refreshToken = user.RefreshTokens.Single(x => x.Token == token);
+            if (!refreshToken.IsActive) return null;
+
+            var newRefreshToken = GenerateRefreshToken(ipAddress);
+            refreshToken.Revoked = DateTime.UtcNow;
+            refreshToken.RevokedByIp = ipAddress;
+            refreshToken.ReplacedByToken = newRefreshToken.Token;
+            user.RefreshTokens.Add(newRefreshToken);
+            _context.Update(user);
+            _context.SaveChanges();
+
+            var jwtToken = GenerateJwtToken(user);
+            return new AuthenticateResponse(user, jwtToken, refreshToken.Token);
+        }
+
+        public bool RevokeToken(string token, string ipAddress)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.RefreshTokens.Any(t => t.Token == token));
+            if (user is null) return false;
+
+            var refreshToken = user.RefreshTokens.Single(x => x.Token == token);
+            if (!refreshToken.IsActive) return false;
+
+            refreshToken.Revoked = DateTime.UtcNow;
+            refreshToken.RevokedByIp = ipAddress;
+            _context.Update(user);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users;
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        #region Helpers
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        private RefreshToken GenerateRefreshToken(string ipAddress)
+        {
+            using (var rngCryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                var randomBytes = new byte[64];
+                rngCryptoServiceProvider.GetBytes(randomBytes);
+                return new RefreshToken
+                {
+                    Token = Convert.ToBase64String(randomBytes),
+                    Expires = DateTime.UtcNow.AddDays(7),
+                    Created = DateTime.UtcNow,
+                    CreatedByIp = ipAddress
+                };
+            }
+        }
+        #endregion
     }
 }
