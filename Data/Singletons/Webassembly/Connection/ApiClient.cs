af4afdb3b591c13104b4b1717e18d4ac8feb26d3
Theodor Risager <theodor349@gmail.com>
2021-01-22 09:53:09 +0100

    Singleton Works

---
+++ Data/Singletons/Webassembly/Connection/ApiClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webassembly.Connection
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly ILoggedinUser _user;
+
+        public ApiClient(ILoggedinUser user)
+        {
+            _user = user;
+        }
+
+        public async Task Login(string username, string password)
+        {
+            await Task.Delay(100);
+            _user.Username = "Theodor";
+            _user.Token = "This is a token";
+        }
+
+        public ILoggedinUser GetUser()
+        {
+            return _user;
+        }
+    }
+
+    public class LoggedinUser : ILoggedinUser
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Data/Singletons/Webassembly/Connection/IApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Webassembly.Connection
+{
+    public interface IApiClient
+    {
+        ILoggedinUser GetUser();
+        Task Login(string username, string password);
+    }
+}
